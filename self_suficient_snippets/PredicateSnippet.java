4477d58ff19444ce397d0911d9d308ee84ec4b41
disparter <felipe.a.b.faria@gmail.com>
2021-04-03 18:55:22 -0300

    Rename snippet package

---
+++ self_suficient_snippets/PredicateSnippet.java
@@ -0,0 +1,22 @@
+package self_suficient_snippets;
+
+import java.util.*;
+import java.util.function.*;
+
+public class PredicateSnippet {
+    List<String> namedObjects = Arrays.asList("Thing", "Printable", "Stuff");
+
+    public void print(Predicate<String> predicate){
+        for(String object:namedObjects){
+            if(predicate.test(object)){
+                System.out.println(object);
+            }
+        }       
+    }
+
+    public static void main(String[] args) {
+        PredicateSnippet snippet = new PredicateSnippet();
+        snippet.print(o -> "Printable".equals(o));
+    }
+
+}
