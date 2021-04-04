2be1342e741d18958ea5641983b69f5eda3927b8
disparter <felipe.a.b.faria@gmail.com>
2021-04-04 11:11:32 -0300

    Removing auxiliar scripts from module

--- com.disparter.predicate/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-javac -d bin --module-source-path src/main/java $(find src -name "*.java")
--- com.disparter.predicate/test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java --module-path mods --module com.disparter.predicate
