8f486022aac95148878eaab42b145dd82101c314
disparter <felipe.a.b.faria@gmail.com>
2021-04-06 08:06:16 -0300

    Adding output for jar package script

--- modules/package.sh
+++ modules/package.sh
@@ -1,7 +1,8 @@
+mkdir -p out
 for dir in ./src/*/
 do
   module=$(echo "$dir" | cut -c 7-)
-  cmd="jar --create --file=${module%?}@1.0.jar --module-version=1.0 -C mods/$module ."
+  cmd="jar --create --file=out/${module%?}@1.0.jar --module-version=1.0 -C mods/$module ."
   echo $cmd
   $cmd
 done
