2f88abb711558eeff087233f2c5532dad5727380
disparter <felipe.a.b.faria@gmail.com>
2021-04-08 10:18:03 -0300

    Improving package versioning to get from the package.json version

--- modules/package.sh
+++ modules/package.sh
@@ -1,9 +1,10 @@
 mkdir -p ./modules/out
+version=$(grep version package.json | awk -F \" '{print $4}')
 cd modules
 for dir in ./src/*/
 do
   module=$(echo "$dir" | cut -c 7-)
-  cmd="jar --create --file=out/${module%?}@1.0.jar --main-class=${module%?}.Main --module-version=1.0 -C mods/$module ."
+  cmd="jar --create --file=out/${module%?}@$version.jar --main-class=${module%?}.Main --module-version=$version -C mods/$module ."
   echo $cmd
   $cmd
 done
