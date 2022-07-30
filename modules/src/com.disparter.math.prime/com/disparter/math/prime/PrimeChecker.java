52ae78257f5c78c56e89b344eb4b976bff772731
Disparter <felipe.a.b.faria@gmail.com>
2022-07-30 12:41:06 +0100

    Adding a checking for Number Below 2

--- modules/src/com.disparter.math.prime/com/disparter/math/prime/PrimeChecker.java
+++ modules/src/com.disparter.math.prime/com/disparter/math/prime/PrimeChecker.java
@@ -6,6 +6,10 @@ public class PrimeChecker implements Function<Integer, Boolean>{
 
     @Override
     public Boolean apply(Integer number) {
+        if(number < 2){
+            return false;
+        }
+
         for(int i = 2; i < number; i++){
             if(number % i == 0){
                 return false;
